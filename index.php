361501af71b4c0b4402a4f16e846d7516718108c
plotiwitolp <plotiwitolp@gmail.com>
2023-01-25 19:03:31 +0400

    start html

---
+++ index.php
@@ -0,0 +1,168 @@
+<!DOCTYPE html>
+<html lang="ru">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Частный вебмастер Иван Плотников | разработка сайтов под заказ</title>
+    <link rel="shortcut icon" href="favicon.ico" type="image/x-icon">
+    <link rel="stylesheet" href="./assets/fonts/Montserrat/stylesheet.css">
+    <link rel="stylesheet" href="./assets/styles/style.css">
+</head>
+
+<body>
+    <div class="screen">
+        <!-- 1-ый экран - top-banner -->
+        <div class="screen__item">
+            <header>
+                <div class="popup-menu">
+                    <div class="popup-menu__burger">
+                        <span></span>
+                        <span></span>
+                        <span></span>
+                    </div>
+                    <div class="popup-menu__nav">
+                        <ul class="popup-menu__ul">
+                            <li><a href="#">Главная</a></li>
+                            <li><a href="#">Портфолио</a></li>
+                            <li><a href="#">Рассчитать стоимость сайта</a></li>
+                            <li><a href="#">Подать заявку</a></li>
+                            <li><a href="#">О себе</a></li>
+                            <li><a href="#">Контакты</a></li>
+                        </ul>
+                        <div class="popup-menu__closer">
+                            <span></span>
+                            <span></span>
+                        </div>
+                    </div>
+                </div>
+                <div class="regular-menu">
+                    <nav>
+                        <ul>
+                            <li><a href="#">Главная</a></li>
+                            <li><a href="#">Портфолио</a></li>
+                            <li><a href="#">Рассчитать стоимость сайта</a></li>
+                            <li><a href="#">Подать заявку</a></li>
+                            <li><a href="#">О себе</a></li>
+                            <li><a href="#">Контакты</a></li>
+                        </ul>
+                    </nav>
+                </div>
+            </header>
+            <div class="top-banner">
+                <div class="top-banner__text">
+                    <span class="top-banner__hello">
+                        Приветствую! Меня зовут Иван.
+                    </span>
+                    <span class="top-banner__desc1">
+                        Я - частный вебмастер.
+                    </span>
+                    <span class="top-banner__desc2">
+                        Создаю уникальные эффекные сайты под заказ.
+                    </span>
+                </div>
+                <div class="top-banner__img">
+                    <img src="./assets/images/img-03.png" alt="top-banner">
+                </div>
+                <div class="top-banner__line"></div>
+            </div>
+        </div>
+
+        <!-- Мое портфолио -->
+        <div class="screen__item">
+            <h2>Мое портфолио</h2>
+
+            <div class="slider-portfolio">
+                <div class="slider-portfolio__item">
+                    <img src="./assets/images/wellore.png" alt="" class="portfolio__item-bg">
+                    <img src="./assets/images/wellore.png" alt="" class="portfolio__item-front">
+                    <span class="portfolio__item-btn">подробнее</span>
+                </div>
+                <div class="slider-portfolio__item">
+                    <img src="./assets/images/wellore.png" alt="" class="portfolio__item-bg">
+                    <img src="./assets/images/wellore.png" alt="" class="portfolio__item-front">
+                    <span class="portfolio__item-btn">подробнее</span>
+                </div>
+                <div class="slider-portfolio__item">
+                    <img src="./assets/images/wellore.png" alt="" class="portfolio__item-bg">
+                    <img src="./assets/images/wellore.png" alt="" class="portfolio__item-front">
+                    <span class="portfolio__item-btn">подробнее</span>
+                </div>
+                <div class="slider-portfolio__item">
+                    <img src="./assets/images/wellore.png" alt="" class="portfolio__item-bg">
+                    <img src="./assets/images/wellore.png" alt="" class="portfolio__item-front">
+                    <span class="portfolio__item-btn">подробнее</span>
+                </div>
+                <div class="slider-portfolio__item">
+                    <img src="./assets/images/wellore.png" alt="" class="portfolio__item-bg">
+                    <img src="./assets/images/wellore.png" alt="" class="portfolio__item-front">
+                    <span class="portfolio__item-btn">подробнее</span>
+                </div>
+                <div class="slider-portfolio__item">
+                    <img src="./assets/images/wellore.png" alt="" class="portfolio__item-bg">
+                    <img src="./assets/images/wellore.png" alt="" class="portfolio__item-front">
+                    <span class="portfolio__item-btn">подробнее</span>
+                </div>
+                <div class="slider-portfolio__item">
+                    <img src="./assets/images/wellore.png" alt="" class="portfolio__item-bg">
+                    <img src="./assets/images/wellore.png" alt="" class="portfolio__item-front">
+                    <span class="portfolio__item-btn">подробнее</span>
+                </div>
+            </div>
+
+        </div>
+
+        <!-- Подать заявку -->
+        <div class="screen__item">
+            <h2>Подать заявку</h2>
+
+        </div>
+
+        <!-- Расчет стоимости сайта -->
+        <div class="screen__item">
+            <h2>Расчет стоимости сайта</h2>
+
+        </div>
+
+        <!-- 5 шагов при разработке сайта.  -->
+        <div class="screen__item">
+            <h2>5 шагов при разработке сайта.</h2>
+
+        </div>
+
+        <!-- Преимущества частного вебмастера  -->
+        <div class="screen__item">
+            <h2>Преимущества частного вебмастера</h2>
+
+        </div>
+
+        <!-- Отзывы  -->
+        <div class="screen__item">
+            <h2>Отзывы</h2>
+
+        </div>
+
+        <!-- Мои технологии  -->
+        <div class="screen__item">
+            <h2>Мои технологии</h2>
+
+        </div>
+
+        <!-- Часто Задаваемые Вопросы  -->
+        <div class="screen__item">
+            <h2>Часто Задаваемые Вопросы</h2>
+
+        </div>
+
+        <!-- Контакты  -->
+        <div class="screen__item">
+            <h2>Контакты</h2>
+
+        </div>
+    </div>
+    <script src="./assets/scripts/jquery-3.6.0.min.js"></script>
+    <script src="./assets/scripts/script.js"></script>
+</body>
+
+</html>
